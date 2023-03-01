511336d0d67010f2626be8af64dcad7dee4965fa
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-03-01 14:20:09 +0300

    scaffold client

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import './style.css';
+
+function App() {
+  return <div className="bg-red-600 text-4xl">Hello react!</div>;
+}
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = (argv) => {
+  let currentMode = argv.mode;
+
+  return {
+    entry: './src/main.tsx',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+    },
+    plugins: [
+      new Dotenv({
+        path: `./.env.${currentMode}`,
+      }),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'build'),
+      },
+      port: 3000,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          loader: 'ts-loader',
+        },
+        {
+          test: /\.css$/i,
+          include: path.resolve(__dirname, 'src'),
+          use: ['style-loader', 'css-loader', 'postcss-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    },
+  };
+};
