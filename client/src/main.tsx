33aa5b2aadf10533fd698c3af5c5d6ef547c2b78
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-03-01 14:25:02 +0300

    add redux store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './app/store';
 import './style.css';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
