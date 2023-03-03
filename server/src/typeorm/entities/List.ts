f90930d0a649e84fcb9d799ae82fd572964a94b1
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-03-03 10:56:21 +0300

    create list and todo entities

---
+++ server/src/typeorm/entities/List.ts
@@ -0,0 +1,25 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Todo } from './Todo';
+
+@Entity({
+  name: 'lists',
+})
+export class List {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'longtext' })
+  title: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: string;
+
+  @OneToMany(() => Todo, (todos) => todos.todoList)
+  todos: Todo[];
+}
---
+++ server/src/typeorm/entities/Todo.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { List } from './List';
+
+@Entity({
+  name: 'todos',
+})
+export class Todo {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'longtext' })
+  title: string;
+
+  @Column({ type: 'longtext', default: null })
+  description: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: string;
+
+  @ManyToOne(() => List, (todoList) => todoList.todos, {
+    onDelete: 'CASCADE',
+  })
+  todoList: List;
+}
